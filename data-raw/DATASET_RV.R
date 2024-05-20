daddb861e573e7ad06a8d22f98aebad08788928e
smeekes <s.smeekes@maastrichtuniversity.nl>
2024-05-20 12:30:00 +0200

    Updated readme - Removed dependence on HARModel

--- data-raw/DATASET_RV.R
+++ data-raw/DATASET_RV.R
@@ -1,8 +1,10 @@
 ## code to prepare `DATASET_RV` dataset goes here
-library(HARModel)
+if (!requireNamespace("HARModel", quietly = TRUE)) {
+  stop("Cannot simulate as package HARModel not installed.")
+}
 sample_RV<-matrix(NA,200,30)
 for(i in 1:30){
-  sim<-HARSimulate(len=200, periods = c(1, 5, 22),
+  sim<-HARModel::HARSimulate(len=200, periods = c(1, 5, 22),
                    coef = c(0.01, 0.36 ,0.28 , 0.28), errorTermSD = 0.001)
   sample_RV[,i]<-sim@simulation
 }
